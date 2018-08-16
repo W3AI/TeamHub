8332b93931fbe61efdfd076114e0e7d960ee7169
stefian <v_ianta@yahoo.com>
2018-08-16 15:12:43 -0400

    splitted navigation code into header and sidenav-list

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav-list',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav-list.component.css']
 })
 export class SidenavListComponent implements OnInit {
+   @Output() closeSidenav = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onClose() {
+    this.closeSidenav.emit();
+  }
 }
