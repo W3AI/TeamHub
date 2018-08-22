3884da2d6edc7a0b0cbf800632da91e039415ab9
stefian <v_ianta@yahoo.com>
2018-08-21 21:37:11 -0400

    added a separate routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { WelcomeComponent } from './welcome/welcome.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { LoginComponent } from './auth/login/login.component';
 import { ProjectComponent } from './project/project.component';
 import { SkillComponent } from './skill/skill.component';
 import { VentureComponent } from './venture/venture.component';
@@ -11,8 +9,6 @@ import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
     { path: '', component: WelcomeComponent },
-    { path: 'signup', component: SignupComponent},
-    { path: 'login', component: LoginComponent},
     { path: 'skill', component: SkillComponent, canActivate: [AuthGuard]},
     { path: 'project', component: ProjectComponent, canActivate: [AuthGuard]},
     { path: 'venture', component: VentureComponent, canActivate: [AuthGuard]}
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,13 +5,15 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { SharedModule } from '../shared/shared.module';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
     declarations: [SignupComponent, LoginComponent],  
     imports: [
         ReactiveFormsModule,
         AngularFireAuthModule,
-        SharedModule
+        SharedModule,
+        AuthRoutingModule
     ]
 })
 export class AuthModule {}
