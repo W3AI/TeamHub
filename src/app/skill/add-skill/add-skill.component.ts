d62ab6939b3e3d04f53befb8dc63278fe7315973
stefian <v_ianta@yahoo.com>
2018-08-31 20:58:07 -0400

    configured Add form & button for add Prj/Skill/Vent

--- src/app/skill/add-skill/add-skill.component.ts
+++ src/app/skill/add-skill/add-skill.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-skill',
@@ -12,4 +13,7 @@ export class AddSkillComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
 }
--- src/app/venture/add-venture/add-venture.component.ts
+++ src/app/venture/add-venture/add-venture.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-venture',
@@ -12,4 +13,7 @@ export class AddVentureComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
 }
