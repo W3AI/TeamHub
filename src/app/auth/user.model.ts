5aee32af0585f25a3afd9920602ced6d716fae68
stefian <v_ianta@yahoo.com>
2018-09-04 20:01:47 -0400

    additional user fields

--- src/app/auth/user.model.ts
+++ src/app/auth/user.model.ts
@@ -1,8 +1,11 @@
 export interface User {
     email: string;
     userId: string;
+    displayName?: string;
     firstName?: string;
     lastName?: string;
+    photoURL?: string;
+    website?: string;
     country?: string;
     email2?: string;
     dateSignup?: Date;
