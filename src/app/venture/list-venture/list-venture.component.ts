8bd87c2e66d878dec7d1b41809c5c5418bb6ea62
stefian <v_ianta@yahoo.com>
2018-09-07 15:31:51 -0400

    added list venture and TeamWork title

---
+++ src/app/venture/list-venture/list-venture.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-venture',
+  templateUrl: './list-venture.component.html',
+  styleUrls: ['./list-venture.component.css']
+})
+export class ListVentureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
