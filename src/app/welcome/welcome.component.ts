8500cb7e9c2b49c7d4afaa0bf62276828b4709cf
stefian <v_ianta@yahoo.com>
2018-09-14 15:16:45 -0400

    added counters on welcome page

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  // TODO - Build Account Setup page to setup like these
+  friendsNo = 532134;
+  projectsNo = 1364;
+  servicesNo = 2734;
+  realtimeTx = 1567;
+  realtime = false;
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  onStartRealtime() {
+    this.realtime = true;
+
+    this.realtimeTx = 100;
+
+    // while(this.realtime) {
+    //   setTimeout(() => {
+    //     this.realtimeTx = this.realtimeTx + Math.floor(Math.random() * Math.floor(10));
+    //   }, Math.floor(Math.random() * Math.floor(1000)));
+    // }
+  }
+
+  onStopRealtime() {
+    this.realtime = false;
+
+    this.realtimeTx = 200;
   }
 
 }
