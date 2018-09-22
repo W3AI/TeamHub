9aa2417dba34e87f1b6bd5d61e198998a64b2476
stefian <v_ianta@yahoo.com>
2018-09-22 10:42:58 -0400

    added the entity constructor

--- src/app/logic/Context.ts
+++ src/app/logic/Context.ts
@@ -13,14 +13,29 @@ export class Entity {
     name: string;
 
     entityCells: Array<string>;
-
     properties: Array<Property>;
+    values: Array<string>;
+
+    entityMap: Map<string, any>;
 
     constructor(entityRow: string) {
         console.log('Servus from the Entity constructor');
-
         this.entityCells = ops.tokens(entityRow);
 
+        console.log(this.entityCells);
+
+        // To Initialize header - for now type and name 
+
+        // Set the entityMap
+        this.entityMap = new Map();
+        console.log(this.entityMap);
+        console.log(this.entityCells.length);
+        // Below 6 is Currently the index of the first property
+        // 3 is the increment to the next property name
+        for (let i = 6; i <= this.entityCells.length - 3; i = i + 3) {
+            this.entityMap.set(this.entityCells[i], this.entityCells[i + 2]);
+        }
+        console.log(this.entityMap.size);
     }
 }
 
@@ -51,6 +66,5 @@ export class Context {
             this.entityRows.push(ops.pipes(contextRows[i]));
             this.entities.push(new Entity(this.entityRows[i-1]));
         }
-
     }
 }
