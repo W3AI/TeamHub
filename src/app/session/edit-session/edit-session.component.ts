ae71fc081b22d6f28136703d99052b7aa13eec54
stefian <v_ianta@yahoo.com>
2018-09-28 15:24:47 -0400

    edit-session defaults and styles

--- src/app/session/edit-session/edit-session.component.ts
+++ src/app/session/edit-session/edit-session.component.ts
@@ -6,7 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-session.component.css']
 })
 export class EditSessionComponent implements OnInit {
+  defaultProject = `INPUT	3	Terms	:	CAD	,	cent	:	10	,	Seconds	:	60				
+  Jar	,	name	:	Jar3L	,	volume	:	3	,	content	:	0	,	available	 = 	3
+  Jar	,	name	:	Jar5L	,	volume	:	5	,	content	:	0	,	available	 = 	5
+  Jar	,	name	:	Jar8L	,	volume	:	8	,	content	:	8	,	available	 = 	0
+                                  
+  SOLUTIONS
   
+  OUTPUT	1
+  Jar				WITH		content	 = 	4`;
+
+  defaultOperation = `INPUT	2	Terms	:	CAD	,	cent	:	1	, 	Seconds	:	2
+  Jar	1	name	:	from_Jar	,	content	>	0				
+  Jar	2	name	:	to_Jar	,	available	>	0				
+                          
+  FUNCTIONS												
+  poured	 = 	MIN	(	from_Jar	.	content	,	to_Jar	.	available	)	
+                          
+  OUTPUT	2											
+  from_Jar	.	content	-=	poured	,	available	 +=	poured				
+  to_Jar	.	content	 +=	poured	,	available	-=	poured				`;
+
+
   // TODO - [ ] Change oddNumbers[] to Problem Class structures:
   // ContextsScripts[]
   // SolutionScripts[] 
