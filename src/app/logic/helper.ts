5f6ed18dd8c33a5a07a9c02b1119f0f795273f07
stefian <v_ianta@yahoo.com>
2018-10-19 14:46:12 -0400

    added lines to compute stateStamp and statesMemory in rnaCode

--- src/app/logic/helper.ts
+++ src/app/logic/helper.ts
@@ -42,27 +42,27 @@ function tokens(str: string): Array<any> {
     return array;
 }
 
-/** https://gist.github.com/hyamamoto/fd435505d29ebfa3d9716fd2be8d42f0
- * Returns a hash code for a string.
- * (Compatible to Java's String.hashCode())
- *
- * The hash code for a string object is computed as
- *     s[0]*31^(n-1) + s[1]*31^(n-2) + ... + s[n-1]
- * using number arithmetic, where s[i] is the i th character
- * of the given string, n is the length of the string,
- * and ^ indicates exponentiation.
- * (The hash value of the empty string is zero.)
- *
- * @param {string} s a string
- * @return {number} a hash code value for the given string.
- */
-function hashCode(s: string): number {
-    var h = 0, l = s.length, i = 0;
-    if ( l > 0 )
-      while (i < l)
-        h = (h << 5) - h + s.charCodeAt(i++) | 0;
-    return h;
-  };
+// /** https://gist.github.com/hyamamoto/fd435505d29ebfa3d9716fd2be8d42f0
+//  * Returns a hash code for a string.
+//  * (Compatible to Java's String.hashCode())
+//  *
+//  * The hash code for a string object is computed as
+//  *     s[0]*31^(n-1) + s[1]*31^(n-2) + ... + s[n-1]
+//  * using number arithmetic, where s[i] is the i th character
+//  * of the given string, n is the length of the string,
+//  * and ^ indicates exponentiation.
+//  * (The hash value of the empty string is zero.)
+//  *
+//  * @param {string} s a string
+//  * @return {number} a hash code value for the given string.
+//  */
+// function hashCode(s: string): number {
+//     var h = 0, l = s.length, i = 0;
+//     if ( l > 0 )
+//       while (i < l)
+//         h = (h << 5) - h + s.charCodeAt(i++) | 0;
+//     return h;
+//   };
 
   function buildTableHeader(tableId: string, array: any[]) {
     let table = <HTMLTableElement>document.getElementById(tableId);
@@ -117,4 +117,4 @@ function updateTxNo(txNo) {
   }
 
 // export all helper functions
-export { rn, lines, pipes, tokens, hashCode, buildTableHeader, addTableRow, addContextToMemoryTable, updateNodesNo, updateTxNo };
+export { rn, lines, pipes, tokens, buildTableHeader, addTableRow, addContextToMemoryTable, updateNodesNo, updateTxNo };
