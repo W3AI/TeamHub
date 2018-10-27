0a1bb2fba0e66ff80a6330cdaf6cfea23ba3a1dd
stefian <v_ianta@yahoo.com>
2018-10-27 11:54:44 -0400

    added the ol code for the map component

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import OlMap from 'ol/Map';
+import OlXYZ from 'ol/source/XYZ';
+import OlTileLayer from 'ol/layer/Tile';
+import OlView from 'ol/View';
+
+import { fromLonLat } from 'ol/proj';
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -7,9 +14,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  map: OlMap;
+  source: OlXYZ;
+  layer: OlTileLayer;
+  view: OlView;
+
   constructor() { }
 
-  ngOnInit() {
+ngOnInit() {
+    this.source = new OlXYZ({
+      url: 'http://tile.osm.org/{z}/{x}/{y}.png'
+    });
+
+    this.layer = new OlTileLayer({
+      source: this.source
+    });
+
+    this.view = new OlView({
+      center: fromLonLat([6.661594, 50.433237]),
+      zoom: 3
+    });
+
+    this.map = new OlMap({
+      target: 'map',
+      layers: [this.layer],
+      view: this.view
+    });
   }
 
 }
