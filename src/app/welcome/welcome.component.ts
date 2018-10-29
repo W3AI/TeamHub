8e5021c3ccc5437d196aabfa97f065805762d3a7
stefian <v_ianta@yahoo.com>
2018-10-29 10:56:24 -0400

    added the spinner loop code to welcome constructor

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -11,6 +11,8 @@ import * as q from "../logic/AlgoQueue";
 export class WelcomeComponent implements OnInit, AfterViewInit {
 
   timer: number;
+  tagsCounter: number;
+  i: number = 0;
 
   // TODO - Build Account Setup page to setup like these
 
@@ -37,54 +39,23 @@ export class WelcomeComponent implements OnInit, AfterViewInit {
   link6: string = '';
   link7: string = '';
 
+  nProject1: string = '';
+  nProject2: string = '';
+  nProject3: string = '';
+  nProject4: string = '';
+  nProject5: string = '';
+  nProject6: string = '';
+  nProject7: string = '';
+
+  nService1: string = '';
+  nService2: string = '';
+  nService3: string = '';
+  nService4: string = '';
+  nService5: string = '';
+  nService6: string = '';
+  nService7: string = '';
+
   constructor() {
-
-    // this.timer = setInterval( ()=> {
-
-    //   // Super Neural Net Teams
-    //   this.superFriendsNo = this.superFriendsNo + Math.floor(Math.random() * Math.floor(4))
-    //     - Math.floor(Math.random() * Math.floor(2));
-
-    //   this.superProjectsNo = this.superProjectsNo + Math.floor(Math.random() * Math.floor(3))
-    //     - Math.floor(Math.random() * Math.floor(2));
-
-    //   this.superServicesNo = this.superServicesNo + Math.floor(Math.random() * Math.floor(5))
-    //     - Math.floor(Math.random() * Math.floor(2));
-
-    //   this.superTxPerMs = this.superTxPerMs + Math.floor(Math.random() * Math.floor(3))
-    //     - Math.floor(Math.random() * Math.floor(2));
-
-    //   this.superRatePerMin = this.superRatePerMin + (Math.floor(Math.random() * Math.floor(3))
-    //     - Math.floor(Math.random() * Math.floor(2))) / 100;
-
-
-    //   // Office Teams
-    //   this.officeFriendsNo = this.officeFriendsNo + Math.floor(Math.random() * Math.floor(2))
-    //     - Math.floor(Math.random() * Math.floor(2));
-
-    //   this.officeProjectsNo = this.officeProjectsNo + Math.floor(Math.random() * Math.floor(2))
-    //     - Math.floor(Math.random() * Math.floor(2));
-
-    //   this.officeServicesNo = this.officeServicesNo + Math.floor(Math.random() * Math.floor(2))
-    //     - Math.floor(Math.random() * Math.floor(2));
-
-    //   this.officeTxPerMs = this.officeTxPerMs + Math.floor(Math.random() * Math.floor(2))
-    //     - Math.floor(Math.random() * Math.floor(2));
-
-    //   this.officeRatePerMin = this.officeRatePerMin + (Math.floor(Math.random() * Math.floor(2))
-    //     - Math.floor(Math.random() * Math.floor(2))) / 100;
-
-    // }, 500);
-
-  }
-
-  ngOnInit() {
-  }
-
-  ngAfterViewInit() {
-    // testing writing in the loop table from the queue matric
-    // h.setCell('loop', 3, 3, 'test loop');
-    // h.setCell('loop', 4, 4, q.queue[3][0]);
 
     // Translating gs code from function intlTeams() from WorldMarket.gs / W3AI spreadsheet / @W3AI.net 
     // 14 - Starting from line 14 - skipping the gs files initializations
@@ -115,7 +86,7 @@ export class WelcomeComponent implements OnInit, AfterViewInit {
     tag[t][0] = q.queue[t][0]; // Read into tag[] the link value from Column 1 / A
     tag[t][1] = q.queue[t][2]; // Read into tag[] the nr problems/tag value from Column 3 / C
     tag[t][2] = q.queue[t][17]; // Read into tag[] the nr services/tag value from Column 18 / R
-    
+
     // Jun 13 SI: I just added 7 columns for countries of the originator of each problem and service 
     problems[t] = new Array(tag[t][1]);
     pFlags[t] = new Array(tag[t][1]);
@@ -132,19 +103,160 @@ export class WelcomeComponent implements OnInit, AfterViewInit {
     }
   } // End loading the arrays for Tags, Problems, Services and Flags for Problems and services
 
+
+    this.timer = setInterval( ()=> {
+
+      // Write the loop's 7 tags
+      this.link1 = tag[(this.i + 0) % nrLinks][0];
+      this.link2 = tag[(this.i + 1) % nrLinks][0];
+      this.link3 = tag[(this.i + 2) % nrLinks][0];
+      this.link4 = tag[(this.i + 3) % nrLinks][0];
+      this.link5 = tag[(this.i + 4) % nrLinks][0];
+      this.link6 = tag[(this.i + 5) % nrLinks][0];
+      this.link7 = tag[(this.i + 6) % nrLinks][0];
+
+      // Write the Titles of the Projects associated with the mid Tag / Link - nr 4
+      this.nProject1 = problems[(this.i + 3) % nrLinks][1];
+      this.nProject2 = problems[(this.i + 3) % nrLinks][2];
+      this.nProject3 = problems[(this.i + 3) % nrLinks][3];
+      this.nProject4 = problems[(this.i + 3) % nrLinks][4];
+      this.nProject5 = problems[(this.i + 3) % nrLinks][5];
+      this.nProject6 = problems[(this.i + 3) % nrLinks][6];
+      this.nProject7 = problems[(this.i + 3) % nrLinks][7];
+
+      // Write the Titles of the Services associated with the mid Tag / Link - nr 4
+      this.nService1 = services[(this.i + 3) % nrLinks][1];
+      this.nService2 = services[(this.i + 3) % nrLinks][2];
+      this.nService3 = services[(this.i + 3) % nrLinks][3];
+      this.nService4 = services[(this.i + 3) % nrLinks][4];
+      this.nService5 = services[(this.i + 3) % nrLinks][5];
+      this.nService6 = services[(this.i + 3) % nrLinks][6];
+      this.nService7 = services[(this.i + 3) % nrLinks][7];
+
+      // Move to next tag / link in the queue
+      this.i++;
+
+      // Super Neural Net Teams
+      this.superFriendsNo = this.superFriendsNo + Math.floor(Math.random() * Math.floor(4))
+        - Math.floor(Math.random() * Math.floor(2));
+      this.superProjectsNo = this.superProjectsNo + Math.floor(Math.random() * Math.floor(3))
+        - Math.floor(Math.random() * Math.floor(2));
+      this.superServicesNo = this.superServicesNo + Math.floor(Math.random() * Math.floor(5))
+        - Math.floor(Math.random() * Math.floor(2));
+      this.superTxPerMs = this.superTxPerMs + Math.floor(Math.random() * Math.floor(3))
+        - Math.floor(Math.random() * Math.floor(2));
+      this.superRatePerMin = this.superRatePerMin + (Math.floor(Math.random() * Math.floor(3))
+        - Math.floor(Math.random() * Math.floor(2))) / 100;
+
+      // Office Teams
+      this.officeFriendsNo = this.officeFriendsNo + Math.floor(Math.random() * Math.floor(2))
+        - Math.floor(Math.random() * Math.floor(2));
+      this.officeProjectsNo = this.officeProjectsNo + Math.floor(Math.random() * Math.floor(2))
+        - Math.floor(Math.random() * Math.floor(2));
+      this.officeServicesNo = this.officeServicesNo + Math.floor(Math.random() * Math.floor(2))
+        - Math.floor(Math.random() * Math.floor(2));
+      this.officeTxPerMs = this.officeTxPerMs + Math.floor(Math.random() * Math.floor(2))
+        - Math.floor(Math.random() * Math.floor(2));
+      this.officeRatePerMin = this.officeRatePerMin + (Math.floor(Math.random() * Math.floor(2))
+        - Math.floor(Math.random() * Math.floor(2))) / 100;
+
+    }, 300);
+
+  }
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+  //   // testing writing in the loop table from the queue matric
+  //   // h.setCell('loop', 3, 3, 'test loop');
+  //   // h.setCell('loop', 4, 4, q.queue[3][0]);
+
+  //   // Translating gs code from function intlTeams() from WorldMarket.gs / W3AI spreadsheet / @W3AI.net 
+  //   // 14 - Starting from line 14 - skipping the gs files initializations
+  //   let nrLinks = q.queue.length;
+  //   console.log('-- Queue Length: ' + nrLinks);
+
+  //   // 15 - TODO - [ ] - add top header line to the matrix? including the nrProblems and Services
+  //   let nrProblems = 134;
+  //   let nrServices = 43;
+
+  //   // 24 
+  //   let spikes = 7;     // Nr rows in the market wheel in the W3AI sheet - here called spikes
+  //   // 28
+  //   let row0 = 0;       // to keep some similarity / compatibility with the gs code
+
+  //   // TODO - [ ] - to review and change vars declarations below that are copied from the WorldMarket.gs file
+  //   // 34 -  Define and load the tags array[][] / [tag, nr problems/tag, nr services/tag]
+  //   let tag = new Array(nrLinks);
+  //   let problems = new Array(nrLinks);
+  //   let pFlags = new Array(nrLinks);   // Array with Country codes for Problems
+  //   var services = new Array(nrLinks);
+  //   var sFlags = new Array(nrLinks);   // Array with Country codes for Services
+
+  //   // 41 - 3 is removed now - The 3 in Rows (3 + t) is the header offset - the tags/links start from line/row 3 - Jun 10, 2018       
+  //   for (let t=0; t<nrLinks; t++) {
+            
+  //   tag[t] = new Array(3); // to write 3 values 
+  //   tag[t][0] = q.queue[t][0]; // Read into tag[] the link value from Column 1 / A
+  //   tag[t][1] = q.queue[t][2]; // Read into tag[] the nr problems/tag value from Column 3 / C
+  //   tag[t][2] = q.queue[t][17]; // Read into tag[] the nr services/tag value from Column 18 / R
+
+  //   // Jun 13 SI: I just added 7 columns for countries of the originator of each problem and service 
+  //   problems[t] = new Array(tag[t][1]);
+  //   pFlags[t] = new Array(tag[t][1]);
+  //   for(let p=0; p<tag[t][1]; p++) {
+  //     problems[t][p] = q.queue[t][10+p]; // demand.getRange(t, 11 + p).getValue(); // Problems Titles start in col 11 / K
+  //     pFlags[t][p] = q.queue[t][3+p];   // demand.getRange(t, 4 + p).getValue(); // Problems' Flags start in col 4 / D
+  //   }
+    
+  //   services[t] = new Array(tag[t][2]);
+  //   sFlags[t] = new Array(tag[t][2]);
+  //   for(let s=0; s<tag[t][2]; s++) {
+  //     services[t][s] = q.queue[t][25+s]; // demand.getRange(t, 26 + s).getValue();  // Services start in col 26 / Z
+  //     sFlags[t][s] = q.queue[t][18+s]; // demand.getRange(t, 19 + p).getValue(); // Problems' Flags start in col 19 / S
+  //   }
+  // } // End loading the arrays for Tags, Problems, Services and Flags for Problems and services
+
+
+
   // 65 - Set show nrProblems and nrServices - this will have to be updated later from Firestore in realtime afteeach cycle?
   // TODO - [ ] - move into the interval loop once updated from Firestore
-  h.setCell('loop', 2, 5, 'Market Browsing');   // to comment this !!!
-  h.setCell('loop', 2, 2, nrProblems);          // initially 'nProjects'
-  h.setCell('loop', 2, 8, nrServices);          // initially 'nGenes'
+  // h.setCell('loop', 2, 5, 'Market Browsing');   // to comment this !!!
+  // h.setCell('loop', 2, 2, nrProblems);          // initially 'nProjects'
+  // h.setCell('loop', 2, 8, nrServices);          // initially 'nGenes'
 
   // 69 - var urlFlags = "https://tcp1pnet.files.wordpress.com/2018/06/";
-  let urlFlags = 'assets/flags/';
+  // let urlFlags = 'assets/flags/';
+
+  // console.log('-- tag[][]: ');
+  // console.log(tag);
 
   // 71 - Now Start the Market Wheel
   // TODO - [ ] - To use some modulo nrLinks?
-  let nrCycles = 10;  // TODO - [ ] - To change after fixing the speed
-  let i = 1;
+  // let nrCycles = 10;  // TODO - [ ] - To change after fixing the speed
+  // let i = 1;
+
+  // this.i = 0;
+
+  // while (this.i < nrLinks *nrCycles ) {
+        
+  //       // Write the 7 tags
+  //       this.link1 = tag[(this.i + 0) % nrLinks][0];
+  //       this.link2 = tag[(this.i + 1) % nrLinks][0];
+  //       this.link3 = tag[(this.i + 2) % nrLinks][0];
+  //       this.link4 = tag[(this.i + 3) % nrLinks][0];
+  //       this.link5 = tag[(this.i + 4) % nrLinks][0];
+  //       this.link6 = tag[(this.i + 5) % nrLinks][0];
+  //       this.link7 = tag[(this.i + 6) % nrLinks][0];
+  //       this.i++;
+
+  //     // });
+
+  //   } // END loop on %nrLinks
+
+
+
   // while (i <= nrLinks * nrCycles) {
 
   //     for (let s = 0; s < spikes; s++) {
@@ -177,7 +289,6 @@ export class WelcomeComponent implements OnInit, AfterViewInit {
   //           // market.getRange(row0 + sn, 8).setFormula('=image("'+ urlFlags + sFlags[(i-1+3)%nrLinks][sn] + '.png")'); // display the flag
   //         }
   //       }
-
   //     }
 
   //     h.setCell('loop', row0 + 7, 6, i); // market.getRange(row0 + 7, 6).setValue(i);  
